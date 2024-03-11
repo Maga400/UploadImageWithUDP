4541c9acf1f9070d34782de3438278c5cca506f3
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-03-11 13:17:05 +0400

    Add project files.

---
+++ ClientSide/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ClientSide
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetStart(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(1000);
+            await LoadImage();
+
+        }
+
+        public async Task LoadImage()
+        {
+            Task.Delay(1000);
+            var client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var IP = IPAddress.Parse("127.0.0.1");
+            var port = 27001;
+            EndPoint clientEP = new IPEndPoint(IP, port);
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            var buffer = new byte[1024];
+            var buffer2 = new byte[ushort.MaxValue - 29];
+
+            while (true)
+            {
+                buffer = Encoding.UTF8.GetBytes("Request Servere atildi.Sekil cekilmeye baslayacaq.");
+                await client.SendToAsync(new ArraySegment<byte>(buffer), clientEP);
+
+                var d = Task.Factory.StartNew(() =>
+                {
+
+                    var imageData = client.ReceiveFrom(buffer2, SocketFlags.None, ref remoteEP);
+
+                    using (MemoryStream stream = new MemoryStream(imageData))
+                    {
+                        BitmapImage image = new BitmapImage();
+                        image.BeginInit();
+                        image.StreamSource = stream;
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        image.EndInit();
+
+                        MessageBox.Show(image.Width.ToString());
+
+                        ScreenImage.Source = image;
+
+                    }
+
+                });
+            }
+        }
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var listener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+var IP = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listenerEP = new IPEndPoint(IP, port);
+EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+listener.Bind(listenerEP);
+
+string? message = null;
+var buffer = new byte[ushort.MaxValue - 29];
+int len = 0;
+
+Console.WriteLine($@"Local EP => {listener.LocalEndPoint} ----- Message => Serverimiz ise basladi");
+while (true)
+{
+    var result = await listener.ReceiveFromAsync(new ArraySegment<byte>(buffer), SocketFlags.None, remoteEP);
+
+    len = result.ReceivedBytes;
+    message = Encoding.UTF8.GetString(buffer, 0, len);
+    Console.WriteLine($@"Remote EP => {result.RemoteEndPoint} ----- Message => {message}");
+
+    Task a = Task.Factory.StartNew(() =>
+    {
+
+
+        int screenWidth = 50;
+        int screenHeight = 50;
+
+        using (Bitmap screenshot = new Bitmap(screenWidth, screenHeight))
+        {
+
+            using (Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.FillRectangle(Brushes.White, 0, 0, screenWidth, screenHeight);
+            }
+
+
+            byte[] imageData;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                screenshot.Save(stream, ImageFormat.Png);
+                imageData = stream.ToArray();
+            }
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27001);
+            listener.SendToAsync(new ArraySegment<byte>(imageData), SocketFlags.None, result.RemoteEndPoint);
+            //client.Send(imageData, imageData.Length, endPoint);
+
+        }
+
+    });
+    await a;
+}
+
+
+
+
+
+
+
+
+
+
